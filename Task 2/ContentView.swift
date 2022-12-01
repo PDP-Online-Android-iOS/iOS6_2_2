a86a4e08af9089e5bc5e875eb29f3249a2aac470
OgabekDev <ogabekdev@gmail.com>
2022-12-01 17:59:26 +0500

    All Done

--- Task 2/ContentView.swift
+++ Task 2/ContentView.swift
@@ -8,15 +8,48 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+    @State var welcome = "str_welcome".localized()
+        
+        var body: some View {
+            VStack{
+                VStack{
+                    Text(welcome)
+                }.frame(maxWidth: .infinity, maxHeight: .infinity)
+                
+                HStack(spacing:0){
+                    VStack{
+                        Button(action: {
+                            Bundle.setLanguage(lang: "en")
+                            welcome = "str_welcome".localized()
+                        },label:{
+                            Text("English").foregroundColor(Color.white)
+                                .frame(maxWidth:.infinity, maxHeight: .infinity).background(Color.red)
+                        })
+                    }.frame(maxWidth: .infinity, maxHeight: .infinity)
+                    
+                    VStack{
+                        Button(action: {
+                            Bundle.setLanguage(lang: "ko")
+                            welcome = "str_welcome".localized()
+                        },label:{
+                            Text("Korean").foregroundColor(Color.white)
+                                .frame(maxWidth:.infinity, maxHeight: .infinity).background(Color.blue)
+                        })
+                    }.frame(maxWidth: .infinity, maxHeight: .infinity)
+                    
+                    VStack{
+                        Button(action: {
+                            Bundle.setLanguage(lang: "zh-Hant")
+                            welcome = "str_welcome".localized()
+                        },label:{
+                            Text("Chinese").foregroundColor(Color.white)
+                                .frame(maxWidth:.infinity, maxHeight: .infinity).background(Color.green)
+                        })
+                    }.frame(maxWidth: .infinity, maxHeight: .infinity)
+                    
+                }.frame(maxWidth: .infinity, maxHeight: 70)
+            }
         }
-        .padding()
-    }
 }
 
 struct ContentView_Previews: PreviewProvider {
